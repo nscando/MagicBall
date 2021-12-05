f94d04fd22c04e1ff750905e60783a93f9f86305
nscando <nicolas.scandizzo@gmail.com>
2021-12-05 12:03:10 -0300

    Menu

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,38 @@
+// Aswer values
+const val RESPUESTA_AFIRMATIVA = "✅"
+const val RESPUESTA_NEGATIVA = "❌"
+const val RESPUESTA_DUDOSA = "\uD83E\uDD14"
+
+//Join answers with values
+val respuestas = mapOf(
+    "Sí" to RESPUESTA_AFIRMATIVA,
+    "Es cierto" to RESPUESTA_AFIRMATIVA,
+    "Totalmente" to RESPUESTA_AFIRMATIVA,
+    "Sin duda" to RESPUESTA_AFIRMATIVA,
+    "Pregunta en otro momento" to RESPUESTA_DUDOSA,
+    "No puedo decirte en este momento" to RESPUESTA_DUDOSA,
+    "Puede que si o puede que no" to RESPUESTA_DUDOSA,
+    "No va a suceder" to RESPUESTA_NEGATIVA,
+    "No cuentes con ello" to RESPUESTA_NEGATIVA,
+    "Definitivamente no" to RESPUESTA_NEGATIVA,
+    "No lo creo" to RESPUESTA_NEGATIVA,
+)
+
 fun main(args: Array<String>) {
+
+    println("Hola soy tu Bola 8 Magica, creada en Kotlin. Cual de estas opciones deseas realizar ")
+    println("1. Realizar una Pregunta")
+    println("2. Revisar todas la respuestas")
+    println("3. Salir")
+
+    val valorIngresado = readLine()
+
+    when (valorIngresado) {
+        "1" -> realizarPregunta()
+        "2" -> mostrarRespuestas()
+        "3" -> salir()
+        else -> mostrarError()
+    }
 
 }
 
